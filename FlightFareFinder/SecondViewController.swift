98f27008ec928175ed2dfbad3723f3e328fb0ccc
Andrew Ford <andrew@andrewford.co.nz>
2015-06-20 16:31:12 +1200

    Initial Commit

---
+++ FlightFareFinder/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FlightFareFinder
+//
+//  Created by Andrew Ford on 20/06/15.
+//  Copyright (c) 2015 Andrew Ford. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlightFareFinderTests/FlightFareFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlightFareFinderTests.swift
+//  FlightFareFinderTests
+//
+//  Created by Andrew Ford on 20/06/15.
+//  Copyright (c) 2015 Andrew Ford. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FlightFareFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

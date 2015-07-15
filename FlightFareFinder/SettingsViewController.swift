d671550cba7c1163420cadebec6647f95ed8a536
Andrew Ford <andrew@andrewford.co.nz>
2015-07-15 23:51:13 +1200

    Added settings view and fxform controller. To do add saving of form data.

--- FlightFareFinder/SettingsViewController.swift
+++ FlightFareFinder/SettingsViewController.swift
@@ -8,28 +8,30 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: FXFormViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func awakeFromNib() {
+        
+        formController.form = SettingsForm()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func submitRegistrationForm(cell: FXFormFieldCellProtocol) {
+        
+        //we can lookup the form from the cell if we want, like this:
+        let form = cell.field.form as! SettingsForm
+        
+        //we can then perform validation, etc
+        /*
+        if form.agreedToTerms {
+            
+            UIAlertView(title: "Registration Form Submitted", message: "", delegate: nil, cancelButtonTitle: "OK").show()
+            
+        } else {
+            
+            UIAlertView(title: "User Error", message: "Please agree to the terms and conditions before proceeding", delegate: nil, cancelButtonTitle: "OK", otherButtonTitles: "Yes Sir!").show()
+        }
+        */
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

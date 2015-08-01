2840e8c5818e35f60112bc54d080ac177bc33b52
Andrew Ford <andrew@andrewford.co.nz>
2015-08-01 23:10:39 +1200

    Added table cell colouring for matching match criteria that's set in settings.

--- FlightFareFinder/MyFaresTableViewCell.swift
+++ FlightFareFinder/MyFaresTableViewCell.swift
@@ -15,13 +15,30 @@ class MyFaresTableViewCell: UITableViewCell {
     @IBOutlet weak var priceLabel: UILabel!
     
     func configureWithMyFare(fare: JSON) {
-        let price = fare["@farePrice"].string ?? ""
-        let fareDate = fare["@outboundDate"].string ?? ""        
+        let price: String = fare["@farePrice"].string ?? ""
+        let priceInt: Int? = Int(price)
+        let fareDate = fare["@outboundDate"].string ?? ""
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let fareDateFormatted = dateFormatter.dateFromString(fareDate)
+        let settings = loadUserSettings()
+        backgroundColor = UIColor.whiteColor()
+        
+        if (settings.notificationsEnabled) {
+            print(priceInt)
+            print(settings.alertAmount)
+            if (priceInt < settings.alertAmount) {
+                print(fareDateFormatted)
+                print(settings.dateFrom)
+                print(fareDateFormatted!.timeIntervalSinceDate(settings.dateFrom!))
+                if (fareDateFormatted!.timeIntervalSinceDate(settings.dateFrom!).isSignMinus) {
+                    backgroundColor = UIColor(red:0.29, green:0.86, blue:0.37, alpha:0.5)
+                }
+            }
+        }
         
         dateLabel.text = stringDateToDate(fareDate, dateType: 1)
         priceLabel.text = "$" + price
-//        if () {
-//            backgroundColor = UIColor.redColor()
-//        }
+
     }
 }

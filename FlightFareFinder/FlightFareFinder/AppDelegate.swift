a574faa8d40c4f23cf98689bb41a8f06cd466f99
Andrew Ford <andrew@andrewford.co.nz>
2015-08-06 00:28:41 +1200

    Added Sketch file. Added icons. Updated message for notifications.

--- FlightFareFinder/FlightFareFinder/AppDelegate.swift
+++ FlightFareFinder/FlightFareFinder/AppDelegate.swift
@@ -13,27 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
 
-        UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
-        
         let types = UIUserNotificationType([UIUserNotificationType.Alert, UIUserNotificationType.Sound, UIUserNotificationType.Badge])
         let settings = UIUserNotificationSettings(forTypes: types, categories: nil)
         application.registerUserNotificationSettings(settings)
 
         UIScrollView.appearance().backgroundColor = UIColor.groupTableViewBackgroundColor()
         
-        UITabBar.appearance().tintColor = getAppColor()
-        UINavigationBar.appearance().tintColor = getAppColor()
-        UISegmentedControl.appearance().tintColor = getAppColor()
-        UITableView.appearance().tintColor = getAppColor()
-        UISwitch.appearance().onTintColor = getAppColor()
-        UIRefreshControl.appearance().tintColor = getAppColor()
-        
         return true
     }
-    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -57,44 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
-        
-        var fares: JSON! = []
-        let userDefaults = UserService.loadUserSettings()
-        var bNotificationSent = false
-  
-        // Do call to get fares
-        GASService.getPrices(userDefaults.origin!, destination: userDefaults.destination!) { (JSON) -> () in
-            fares = JSON["PriceAvailability"]
-            
-            if (fares != nil) {
-                for (_, subJson) in JSON["PriceAvailability"] {
-                    let price: String = subJson["@farePrice"].string ?? ""
-                    let priceInt: Int? = Int(price)
-                    
-                    let fareDate = subJson["@outboundDate"].string ?? ""
-                    let dateFormatter = NSDateFormatter()
-                    dateFormatter.dateFormat = "yyyy-MM-dd"
-                    let fareDateFormatted = dateFormatter.dateFromString(fareDate)
-
-                    if ((userDefaults.notificationsEnabled) &&
-                        (priceInt < userDefaults.alertAmount) &&
-                        (userDefaults.dateFrom!.timeIntervalSinceDate(fareDateFormatted!).isSignMinus)) {
-                            if (!bNotificationSent) {
-                                print("addNotification")
-                                // Call FareNotifications.addNotification
-                                FareNotifications.sharedInstance.addNotification("Flight fares availble under $" + price, title: "Flight for " + fareDate)
-                                bNotificationSent = true
-                            }
-                    }
-
-                }
-            }
-
-        }
-        
-        completionHandler(UIBackgroundFetchResult.NewData)
     
-    }
 }
 
